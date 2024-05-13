2442a6707fe5cf8335de3fca18712c410c32378d
Yuri Cruz <douglasy190@gmail.com>
2024-05-13 15:42:25 -0300

    up

--- script.ts
+++ script.ts
@@ -14,11 +14,23 @@ class Item {
     removerBeneficios(){
         
     }
+
+    getNome(){
+        return this.nome;
+    }
 }
 
 class ItemInventario{
     private quantidade: number;
     private item: Item;
+
+    getQuantidade(){
+        return this.quantidade;
+    }
+
+    getItem(){
+        return this.item;
+    }
 }
 
 class Arma extends Item {
@@ -31,32 +43,61 @@ class Pocao extends Item {
 
 class Inventario {
     private itemInventario: ItemInventario[] = [];
-    private quantidadeMaxItens: number;
+    private quantidadeMaxItens: number = 20;
 
     adicionarItem(){
 
     }
+
+    getItensIventario(){
+        return this.itemInventario;
+    }
+
+    getMaxQuantidade(){
+        return this.quantidadeMaxItens
+    }
 }
 
 class ItemMenu {
+    opcao: string;
     textoOpcao: string;
+
+    constructor(indice: string, opao: string){
+        this.opcao = indice;
+        this.textoOpcao = opao;
+    }
+}
+
+class Menu {
+    itens: ItemMenu[]=[];
+
+    constructor(){
+        this.itens.push(new ItemMenu ("1", "Equipar Arma"))
+        this.itens.push(new ItemMenu ("2", "Tomar Poção"))
+    }
 }
 
 class Personagem {
-    nome: string;
-    hp: number;
-    mp: number;
-    forca: number;
-    defesa: number;
-    inventario: Inventario;
-    arma: Arma;
+    private nome: string;
+    private hp: number;
+    private mp: number;
+    private forca: number;
+    private defesa: number;
+    private inventario: Inventario;
+    private arma: Arma;
 
     constructor(parameters) {
         
     }
 
     abrirIventario(){
-
+        console.log('Inventario:');
+        let i = 1;
+        for(const itemInventario of this.inventario.getItensIventario()){
+            console.log(`${i} - ${itemInventario.getItem().getNome()} (${itemInventario.getQuantidade})`);
+            i++;
+        }
+        console.log(`$/${this.inventario.getMaxQuantidade}`);
     }
 
     usarItem(){
